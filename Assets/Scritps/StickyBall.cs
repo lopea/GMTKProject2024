6a56dfa8ee3740d1eea8be23b12faeceabf50408
alyx <alyx@Ellie.com>
2024-08-18 19:31:18 -0700

    Ball

--- Assets/Scritps/StickyBall.cs
+++ Assets/Scritps/StickyBall.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
 using UnityEditorInternal;
 using UnityEngine;
 
 public class StickyBall : MonoBehaviour
 {
+    [ReadOnly] public float averageDistance = 0.0f;
+    private float totalDistance = 0.0f;
+    private int numberOfObjects = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,11 @@ public class StickyBall : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        float distance = Vector3.Distance(transform.position, collision.transform.position);
+      
+        totalDistance += distance;
+        numberOfObjects += 1;
         
+        averageDistance = totalDistance / numberOfObjects;
     }
 }
